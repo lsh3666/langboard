c94373cff2d7046a3f163255f3774b8ba6f94513
dynafer <dynaferkim@gmail.com>
2026-04-02 08:11:21 +0100

    fix: removed unnecessary media type check in file retrieval API

--- src/api/langboard/routes/file/FileApi.py
+++ src/api/langboard/routes/file/FileApi.py
@@ -7,8 +7,6 @@ from langboard_shared.core.storage import Storage
 @AppRouter.api.get("/file/{storage_type}/{storage_name}/{filename}", tags=["General"])
 def get_file(storage_type: str = Path(), storage_name: str = Path(), filename: str = Path()) -> Response:
     media_type, _ = guess_type(filename)
-    if media_type is None:
-        raise ApiException.NotFound_404()
 
     file = Storage.get(storage_type, storage_name, filename)
     if file is None:
