ea77270bca77af5f937a78d22e549eae581dfb50
dynafer <dynaferkim@gmail.com>
2026-04-14 03:15:08 +0100

    fix: fixed import statements in WikiEmptyState component

--- src/ui/src/pages/BoardPage/components/wiki/WikiEmptyState.tsx
+++ src/ui/src/pages/BoardPage/components/wiki/WikiEmptyState.tsx
@@ -1,4 +1,6 @@
-import { Box, Flex, IconComponent } from "@/components/base";
+import Box from "@/components/base/Box";
+import Flex from "@/components/base/Flex";
+import IconComponent from "@/components/base/IconComponent";
 import WikiCreateButton from "@/pages/BoardPage/components/wiki/WikiCreateButton";
 import { useTranslation } from "react-i18next";
 
